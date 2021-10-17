7675ca58c3142ae1f204ae744ebdb62e5bf2b3e8
KomalKrishna12 <komalkrishna016@gmail.com>
2021-10-17 13:34:26 +0530

    even odd

---
+++ EvenOdd.java
@@ -0,0 +1,16 @@
+public class EvenOdd {
+    
+    public static void main(String[] args) {
+        
+        int x = 11;
+        if(x % 2 == 0){
+            System.out.println(x + " even number! ");
+        }
+        else{
+            System.out.println(x + " odd number! ");
+        }
+        System.out.println("This is if else statement.!");
+
+    }
+
+}
