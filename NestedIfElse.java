c86f67aedc28c30271599cb1936c0adc40ac46ec
KomalKrishna12 <komalkrishna016@gmail.com>
2021-10-17 16:22:17 +0530

    nested-if-else

---
+++ NestedIfElse.java
@@ -0,0 +1,19 @@
+public class NestedIfElse {
+    
+    public static void main(String[] args) {
+        
+        int n1 = 30;
+        int n2 = 30;
+        if(n1 == n2){
+            System.out.println(n1 + " is equal to " + n2);
+        }
+        else if(n1 > n2){
+            System.out.println(n1 + " is greater than " + n2);
+        }
+        else{
+            System.out.println(n2 + " is greater than " + n1);
+        }
+
+    }
+
+}
