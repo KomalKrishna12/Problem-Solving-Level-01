7dbe71f8013359395535fef3d590feb08b613984
KomalKrishna12 <komalkrishna016@gmail.com>
2021-10-17 19:56:01 +0530

    digits

---
+++ CountDigit.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class CountDigit {
+    
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        int digit = 0;
+        while(n > 0){
+            n = n/10;
+            digit++;
+        }
+        System.out.println("Digits in number : " + digit);
+        scanner.close();
+
+    }
+
+}
