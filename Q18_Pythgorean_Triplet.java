04604a9285fd8d07c62b8d35976188d0543cf677
KomalKrishna12 <komalkrishna016@gmail.com>
2021-10-20 17:45:55 +0530

    initial commit

---
+++ Q18_Pythgorean_Triplet.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+import javax.lang.model.element.Element;
+
+public class Q18_Pythgorean_Triplet {
+    public static void main(String[] args) {
+        
+        Scanner scn = new Scanner(System.in);
+        int a = scn.nextInt();
+        int b = scn.nextInt();
+        int c = scn.nextInt();
+
+        int max = a;
+        if(max < b){
+            max = b;
+        }
+        if(max < c){
+            max = c;
+        }
+
+        if(max == a){
+            System.out.println((a * a) == ((b * b) + (c * c)));
+        }
+        else if(max == b){
+            System.out.println((b * b) == ((a * a) + (c * c)));
+        }
+        else{
+            System.out.println((c * c) == ((b * b) + (a * a)));
+        }
+        scn.close();
+    }
+}
