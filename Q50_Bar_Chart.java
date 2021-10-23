38729bce526b6416eb489bcd2dc3feb29846663d
KomalKrishna12 <komalkrishna016@gmail.com>
2021-10-23 20:52:53 +0530

    initial commit

---
+++ Q50_Bar_Chart.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Q50_Bar_Chart {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int[] arr = new int[scn.nextInt()];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scn.nextInt();
+        }
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        for (int floor = max; floor >= 1; floor--) {
+            for (int i = 0; i < arr.length; i++) {
+                if(arr[i] >= floor){
+                    System.out.print("*\t");
+                }
+                else{
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+        scn.close();
+    }
+}
