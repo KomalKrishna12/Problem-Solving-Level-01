d336ae61dbdf195e4981495d623e32c2a59c97e5
KomalKrishna12 <komalkrishna016@gmail.com>
2021-10-24 22:33:02 +0530

    initial commit

---
+++ Q63_2D_Array.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Q63_2D_Array {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = scn.nextInt();
+            }
+        }
+        //row = arr.length, column = arr[i].length
+        System.out.println("2D Array : ");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        scn.close();
+    }
+}
