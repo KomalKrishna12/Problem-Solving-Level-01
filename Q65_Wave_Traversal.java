4c42228e03f9a2d0c7b3b5cb010354c692a2543f
KomalKrishna12 <komalkrishna016@gmail.com>
2021-10-25 10:14:46 +0530

    initial commit

--- Q65_Wave_Traversal.java
+++ Q65_Wave_Traversal.java
@@ -1,3 +1,11 @@
+//size = 3 * 4
+//arr -> 11  12  13  14
+//       21  22  23  24
+//       31  32  33  34
+//path -> 11-21-31-32-22-12-13-23-33-34-24-14
+//here first loop will be for column 0 to 3
+//when j is even second loop will be for row 0 to 3
+//when j is odd second loop will be for row 3 to 0
 import java.util.Scanner;
 
 public class Q65_Wave_Traversal{
