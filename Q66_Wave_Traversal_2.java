4cbef90196e8f2b8f90b2b5309b8d39334bbbaf5
KomalKrishna12 <komalkrishna016@gmail.com>
2021-10-25 10:25:07 +0530

    initial commit

---
+++ Q66_Wave_Traversal_2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Q66_Wave_Traversal_2 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = scn.nextInt();
+            }
+        }
+        for (int j = 0; j < arr[0].length; j++) {
+            if(j % 2 == 0){
+                for (int i = arr.length - 1; i >= 0; i--) {
+                    System.out.println(arr[i][j]);
+                }
+            }
+            else{
+                for (int i = 0; i < arr.length; i++) {
+                    System.out.println(arr[i][j]);
+                }
+            }
+        }
+        scn.close();
+    }
+}
