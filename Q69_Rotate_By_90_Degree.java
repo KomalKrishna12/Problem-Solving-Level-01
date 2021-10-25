fc147c50e20218fda05770074a289c231aedfee0
KomalKrishna12 <komalkrishna016@gmail.com>
2021-10-25 14:54:57 +0530

    initial commit

---
+++ Q69_Rotate_By_90_Degree.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Q69_Rotate_By_90_Degree {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                arr[i][j] = scanner.nextInt();
+            }
+        }
+        transpose(arr);
+        display(arr);
+        scanner.close();
+    }
+
+    public static void transpose(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr[0].length; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }  
+        for (int i = 0; i < arr.length; i++) {
+            int li = 0;
+            int ri = arr[i].length - 1;
+            while(li < ri){
+                int temp = arr[i][li];
+                arr[i][li] = arr[i][ri];
+                arr[i][ri] = temp;
+                li++;
+                ri--;
+            }
+        }      
+    }
+
+    public static void display(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
