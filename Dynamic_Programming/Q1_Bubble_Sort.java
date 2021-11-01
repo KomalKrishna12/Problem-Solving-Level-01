36180089b5bb0a06b35d15bfef47ee4a21d3f04e
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-01 13:44:00 +0530

    initial commit

---
+++ Dynamic_Programming/Q1_Bubble_Sort.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Q1_Bubble_Sort {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        scanner.close();
+    }
+}
