ecf2db6778a5d46dfc576546e34d1c3dc3106c3d
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-02 13:05:36 +0530

    in

---
+++ Dynamic_Programming/time_and_space_complexity/Q7_Quick_Sort.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Q7_Quick_Sort {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scn.nextInt();
+        }
+        print(arr);
+        scn.close();
+    }
+    public static void print(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
