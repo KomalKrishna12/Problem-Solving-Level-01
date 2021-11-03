563f17c58ac6dd2065a402ef4070c98bd8108119
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-03 22:37:50 +0530

    initial commit

--- Dynamic_Programming/time_and_space_complexity/Q7_Quick_Sort.java
+++ Dynamic_Programming/time_and_space_complexity/Q7_Quick_Sort.java
@@ -8,13 +8,42 @@ public class Q7_Quick_Sort {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = scn.nextInt();
         }
-        quickSort(arr);
+        quickSort(arr,0,arr.length-1);
         print(arr);
         scn.close();
     }
-    public static void quickSort(int[] arr) {
-        
+
+    public static void swap(int[] arr, int i, int j) {
+        System.out.println("Swapping " + arr[i] + " and " + arr[j]);
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
+
+    public static int partition(int[] arr, int pivot, int low, int high) {
+        int i = low;
+        int j = low;
+        while (i <= high) {
+            if (arr[i] <= pivot) {
+                swap(arr, i, j);
+                i++;
+                j++;
+            } else {
+                i++;
+            }
+        }
+        return j - 1;
+    }
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if(low >= high){
+            return;
+        }
+        int pivot = partition(arr, arr[high], low, high);
+        quickSort(arr, low, pivot - 1);
+        quickSort(arr, pivot + 1, high);
+    }
+
     public static void print(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i] + " ");
