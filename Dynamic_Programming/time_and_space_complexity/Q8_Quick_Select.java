5bcfbe68e6264deea6768a0be89ec6aab5759ad0
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-04 11:51:43 +0530

    initial commit

--- Dynamic_Programming/time_and_space_complexity/Q8_Quick_Select.java
+++ Dynamic_Programming/time_and_space_complexity/Q8_Quick_Select.java
@@ -8,16 +8,19 @@ public class Q8_Quick_Select{
         for (int i = 0; i < arr.length; i++) {
             arr[i] = scn.nextInt();
         }
+        display(arr);
         scn.close();
     }
     public static void swap(int[] arr, int i, int j) {
+        System.out.println("Swapping " + arr[i] + " and " + arr[j]);
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
-    public static void print(int[] arr) {
+    public static void display(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
+        System.out.println();
     }
 }
