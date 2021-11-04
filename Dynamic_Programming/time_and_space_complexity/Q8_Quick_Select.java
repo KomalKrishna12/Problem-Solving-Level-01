c51714f35477dd336b698e36bae50e401ec7ff1c
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-04 10:48:48 +0530

    initial commit

---
+++ Dynamic_Programming/time_and_space_complexity/Q8_Quick_Select.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Q8_Quick_Select{
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scn.nextInt();
+        }
+        scn.close();
+    }
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public static void print(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
