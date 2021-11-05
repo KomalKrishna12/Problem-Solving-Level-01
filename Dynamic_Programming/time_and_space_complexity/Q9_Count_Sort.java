5d96decbe21ba26bc82102298546fa00cb86500d
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-05 10:17:05 +0530

    initial commit

---
+++ Dynamic_Programming/time_and_space_complexity/Q9_Count_Sort.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Q9_Count_Sort {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+        scn.close();
+    }
+}
