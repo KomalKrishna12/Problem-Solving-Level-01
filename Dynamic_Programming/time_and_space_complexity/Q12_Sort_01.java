25106d45379e75dbca82447b6dc38d11b4edddd9
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-06 20:45:04 +0530

    initial commit

--- Dynamic_Programming/time_and_space_complexity/Q12_Sort_01.java
+++ Dynamic_Programming/time_and_space_complexity/Q12_Sort_01.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+//To Sort 0's and 1's we use concept of partioning.
 public class Q12_Sort_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -18,6 +18,10 @@ public class Q12_Sort_01 {
         arr[i] = arr[j];
         arr[j] = temp;
     }
+    //this is partioning an array using pivot but here we use 0's and 1's instead of pivot because
+    //array is filled with 0's and 1's. firstly we place both i and j pointer to 0th index
+    //when arr[i] == 1 we simply increament the value of i with 1
+    //when arr[i] == 0 firstly we swap value of i and j then increament both i and j with 1
     public static void sort(int[] arr) {
         int i = 0;
         int j = 0;
