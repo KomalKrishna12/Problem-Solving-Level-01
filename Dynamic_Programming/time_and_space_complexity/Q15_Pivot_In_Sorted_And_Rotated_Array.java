42a5078e81db853ef01acd9f1a51dfb2c83b976b
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-08 08:28:42 +0530

    initial commit

--- Dynamic_Programming/time_and_space_complexity/Q15_Pivot_In_Sorted_And_Rotated_Array.java
+++ Dynamic_Programming/time_and_space_complexity/Q15_Pivot_In_Sorted_And_Rotated_Array.java
@@ -8,12 +8,21 @@ public class Q15_Pivot_In_Sorted_And_Rotated_Array {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = scn.nextInt();
         }
-        print(arr);
+        System.out.println(findPivot(arr));
         scn.close();
     }
-    public static void print(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+    public static int findPivot(int[] arr) {
+        int low = 0;
+        int high = arr.length - 1;
+        while(low < high){
+            int mid = (low + high)/2;
+            if(arr[mid] < arr[high]){
+                high = mid;
+            }
+            else{
+                low = mid + 1;
+            }
         }
+        return arr[low];
     }
 }
