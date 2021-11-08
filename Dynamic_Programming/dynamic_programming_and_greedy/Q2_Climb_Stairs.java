9fc1211d2252093756a221203e467e22558a5a13
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-08 20:14:48 +0530

    initial commit

---
+++ Dynamic_Programming/dynamic_programming_and_greedy/Q2_Climb_Stairs.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Q2_Climb_Stairs {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        scn.close();
+    }
+}
