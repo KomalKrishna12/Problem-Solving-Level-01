8ce9376ed3cda56aa8a9ed05863f8102de4711cd
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-09 12:27:30 +0530

    initial commit

--- Dynamic_Programming/dynamic_programming_and_greedy/Q2_Climb_Stairs.java
+++ Dynamic_Programming/dynamic_programming_and_greedy/Q2_Climb_Stairs.java
@@ -3,6 +3,25 @@ import java.util.Scanner;
 public class Q2_Climb_Stairs {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        System.out.println(climbStairs(n, new int[n+1]));
         scn.close();
+    }
+    public static int climbStairs(int n, int[] qb) {
+        if(n == 0){
+            return 1;
+        }
+        if(n < 0){
+            return 0;
+        }
+        if(qb[n] != 0){
+            return qb[n];
+        }
+        int nm1 = climbStairs(n-1,qb);
+        int nm2 = climbStairs(n-2,qb);
+        int nm3 = climbStairs(n-3,qb);
+        int cp = nm1 + nm2 + nm3;
+        qb[n] = cp;
+        return cp;
     }
 }
