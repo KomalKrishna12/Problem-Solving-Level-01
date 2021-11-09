e09058068e5bb74f165edaae3925f664d2b2982a
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-09 13:10:32 +0530

    initial commit

--- Dynamic_Programming/dynamic_programming_and_greedy/Q2_Climb_Stairs.java
+++ Dynamic_Programming/dynamic_programming_and_greedy/Q2_Climb_Stairs.java
@@ -4,9 +4,14 @@ public class Q2_Climb_Stairs {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        System.out.println(climbStairs(n, new int[n+1]));
+        // System.out.println(climbStairs(n, new int[n+1]));
+        int cp = countPathTab(n);
+        System.out.println(cp);
         scn.close();
     }
+    //this is the method of dynamic proramming
+    //this is memoization method in which we store values in array of that particular index and before using
+    //recursion firstly we check that index in array is that greater than 0 or not if yes than return value
     public static int climbStairs(int n, int[] qb) {
         if(n == 0){
             return 1;
@@ -23,5 +28,22 @@ public class Q2_Climb_Stairs {
         int cp = nm1 + nm2 + nm3;
         qb[n] = cp;
         return cp;
+    }
+    //second method of dynamic programming is tabulation in which we use array without using recursion
+    public static int countPathTab(int n) {
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        for (int i = 1; i < dp.length; i++) {
+            if(i == 1){
+                dp[i] = dp[i-1];
+            }
+            else if(i == 2){
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+            else{
+                dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+            }
+        }
+        return dp[n];
     }
 }
