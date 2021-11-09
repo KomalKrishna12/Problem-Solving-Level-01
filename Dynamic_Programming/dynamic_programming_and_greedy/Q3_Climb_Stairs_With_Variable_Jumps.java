85956a3d4c49e79c7d8c3fa935ed5f3adff37587
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-09 13:13:36 +0530

    initial commit

---
+++ Dynamic_Programming/dynamic_programming_and_greedy/Q3_Climb_Stairs_With_Variable_Jumps.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Q3_Climb_Stairs_With_Variable_Jumps {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        scn.close();
+    }   
+}
