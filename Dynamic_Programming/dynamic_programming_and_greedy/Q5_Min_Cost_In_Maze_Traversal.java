6ad3655a465c5044b3dcb71e239f873f46a7c1fb
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-10 09:59:31 +0530

    initial commit

---
+++ Dynamic_Programming/dynamic_programming_and_greedy/Q5_Min_Cost_In_Maze_Traversal.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Q5_Min_Cost_In_Maze_Traversal{
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = scn.nextInt();
+            }
+        }
+        scn.close();
+    }
+}
