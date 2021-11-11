705cde95cea91ab6e167657bdfc43f10a2f6bb66
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-11 20:42:47 +0530

    initial commit

--- Dynamic_Programming/dynamic_programming_and_greedy/Q7_Target_Sum_Subset_DP.java
+++ Dynamic_Programming/dynamic_programming_and_greedy/Q7_Target_Sum_Subset_DP.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Q7_Target_Sum_Subset_DP{
     public static void main(String[] args) {
-        
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scn.nextInt();
+        }
+        int tar = scn.nextInt();
+        boolean[][] dp = new boolean[n+1][tar+1];
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[i].length; j++) {
+                if(i==0 && j==0){
+                    dp[i][j] = true;
+                }
+                else if(i==0){
+                    dp[i][j] = false;
+                }
+                else if(j==0){
+                    dp[i][j] = true;
+                }
+                else{
+                    
+                }
+            }
+        }
+        scn.close();
     }
 }
