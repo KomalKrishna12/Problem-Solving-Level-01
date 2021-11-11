8669e7fc82b6153ff9c3f899d47a345fc1e97736
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-11 21:44:27 +0530

    initial commit

--- Dynamic_Programming/dynamic_programming_and_greedy/Q8_Coin_Change_Combination.java
+++ Dynamic_Programming/dynamic_programming_and_greedy/Q8_Coin_Change_Combination.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
 public class Q8_Coin_Change_Combination {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scn.nextInt();
+        }
         scn.close();
     }
 }
