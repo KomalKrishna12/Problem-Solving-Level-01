0808b0ec3d4b46d9c1a7dfae9ecd41db59f7874a
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-15 10:58:33 +0530

    initial commit

--- Dynamic_Programming/dynamic_programming_and_greedy/Q8_Coin_Change_Combination.java
+++ Dynamic_Programming/dynamic_programming_and_greedy/Q8_Coin_Change_Combination.java
@@ -13,13 +13,18 @@ public class Q8_Coin_Change_Combination {
         }
         int amt = scn.nextInt();
         int[] dp = new int[amt+1];
+        //there is always a way to pay 0 that is 1 like we get 2^0 is 1
         dp[0] = 1;
         for(int i = 0; i < arr.length; i++){
+            //first loop is for arr array because we want combination
             for(int j = arr[i]; j < dp.length; j++){
+                //second loop is for dp
                 dp[j] += dp[j - arr[i]];
+                //here we will add dp[j-arr[i]] in dp[j] because we already have a way in dp[j-arr[i]]
             }
         }
         System.out.println(dp[amt]);
+        //at end we display combination of the particular amount
         scn.close();
    }
 }
