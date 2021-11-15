f41b58e10228de2578fd6898863080f096a7634a
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-15 12:46:52 +0530

    initial commit

--- Dynamic_Programming/dynamic_programming_and_greedy/Q9_Coin_Change_Permutations.java
+++ Dynamic_Programming/dynamic_programming_and_greedy/Q9_Coin_Change_Permutations.java
@@ -10,6 +10,12 @@ public class Q9_Coin_Change_Permutations {
         }
         int amt = scn.nextInt();
         int[] dp = new int[amt+1];
+        dp[0] = 1;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = arr[i]; j < dp.length; j++) {
+                dp[j] += dp[j - arr[i]];
+            }
+        }
         System.out.println(dp[amt]);
         scn.close();
     }
