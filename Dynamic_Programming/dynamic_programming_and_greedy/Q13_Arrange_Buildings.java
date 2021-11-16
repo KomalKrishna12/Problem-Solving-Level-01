e8a4427582de1a9d95b8239712d4751592fdf8c4
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-16 21:40:47 +0530

    initial commit

--- Dynamic_Programming/dynamic_programming_and_greedy/Q13_Arrange_Buildings.java
+++ Dynamic_Programming/dynamic_programming_and_greedy/Q13_Arrange_Buildings.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
-
+//this is similar problem like count binary strings but here we are given a road in both side of road
+//n plots are there
+//withing that plots either we can build buiding or we can leave it as space
+//their can't be two consecutive buildings
+//their can be two consecutive space
+//both side plots can be in same way or other
 public class Q13_Arrange_Buildings {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
@@ -13,6 +18,11 @@ public class Q13_Arrange_Buildings {
             oldSpace = newSpace;
         }
         long total = oldBuilding + oldSpace;
+        //here we are using long because if user gave 45 as n that means we have to add 45 no which can be
+        //in size 45 which is of long type that's why we are using long
+        //at end we can print total^2 that means total*total beacuse in both side n plots are there and they
+        //can either be in same order or different so if there is 10 way for one side there is also 10 ways 
+        //for other side so total will bcome 10*10 that is 100
         total = total * total;
         System.out.println(total);
         scn.close();
