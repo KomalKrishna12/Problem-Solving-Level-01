157f02e027775323742d72acf70e1dba675f65ea
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-17 21:24:40 +0530

    initial commit

--- Dynamic_Programming/dynamic_programming_and_greedy/Q17_Paint_House.java
+++ Dynamic_Programming/dynamic_programming_and_greedy/Q17_Paint_House.java
@@ -15,9 +15,9 @@ public class Q17_Paint_House {
         dp[0][1] = arr[0][1];
         dp[0][2] = arr[0][2];
         for (int i = 1; i < dp.length; i++) {
-            dp[i][0] = Math.min(dp[i][1], dp[i][2]) + arr[i][0];
-            dp[i][1] = Math.min(dp[i][2], dp[i][0]) + arr[i][1];
-            dp[i][2] = Math.min(dp[i][1], dp[i][0]) + arr[i][2];
+            dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + arr[i-1][0];
+            dp[i][1] = Math.min(dp[i-1][2], dp[i-1][0]) + arr[i-1][1];
+            dp[i][2] = Math.min(dp[i-1][1], dp[i-1][0]) + arr[i-1][2];
         }
         int ans = Math.min(dp[n-1][0], Math.min(dp[n-1][1], dp[n-1][2]));
         System.out.println(ans);
