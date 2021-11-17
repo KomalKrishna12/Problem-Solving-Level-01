0f0ffb28b03c8c302a56a53968ca3f4271e0e338
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-17 20:40:11 +0530

    initial commit

--- Dynamic_Programming/dynamic_programming_and_greedy/Q16_Maximum_Sum_Non_Adjacent_Elements.java
+++ Dynamic_Programming/dynamic_programming_and_greedy/Q16_Maximum_Sum_Non_Adjacent_Elements.java
@@ -8,6 +8,17 @@ public class Q16_Maximum_Sum_Non_Adjacent_Elements {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = scn.nextInt();
         }
+        int include = arr[0];
+        int exculde = 0;
+        for (int i = 1; i < arr.length; i++) {
+            int newInclude = exculde + arr[i];
+            int newExclude = Math.max(include, exculde);
+
+            include = newInclude;
+            exculde = newExclude;
+        }
+        int ans = Math.max(include, exculde);
+        System.out.println(ans);
         scn.close();
     }
 }
