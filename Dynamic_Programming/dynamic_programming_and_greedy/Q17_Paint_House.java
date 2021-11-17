550017436b12e92a5872b322aef7371eb1a81399
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-17 21:00:02 +0530

    initial commit

--- Dynamic_Programming/dynamic_programming_and_greedy/Q17_Paint_House.java
+++ Dynamic_Programming/dynamic_programming_and_greedy/Q17_Paint_House.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Q17_Paint_House {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
         scn.close();
     }
 }
