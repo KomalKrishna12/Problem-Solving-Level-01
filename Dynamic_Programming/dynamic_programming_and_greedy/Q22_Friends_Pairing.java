c123ac5261b78cbc5429d5ef0bb7c3a28cae1f98
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-18 17:19:22 +0530

    initial commit

---
+++ Dynamic_Programming/dynamic_programming_and_greedy/Q22_Friends_Pairing.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Q22_Friends_Pairing {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        scn.close();
+    }
+}
