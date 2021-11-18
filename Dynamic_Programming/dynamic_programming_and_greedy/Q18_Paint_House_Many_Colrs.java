cad807bcfa99a537ac4fae42e4cb5116ae19262e
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-18 14:07:48 +0530

    initial commit

--- Dynamic_Programming/dynamic_programming_and_greedy/Q18_Paint_House_Many_Colrs.java
+++ Dynamic_Programming/dynamic_programming_and_greedy/Q18_Paint_House_Many_Colrs.java
@@ -12,9 +12,12 @@ public class Q18_Paint_House_Many_Colrs{
             }
         }
         int[][] dp = new int[n][k];
+        //firstly we place cost for first row which will same as arr[0][j]
         for (int i = 0; i < k; i++) {
             dp[0][i] = arr[0][i];
         }
+        //in second loop we craeted a variable min which we compare if every element of last row
+        //if any mininum value find then we store that in min variable
         for (int i = 1; i < dp.length; i++) {
             for (int j = 0; j < dp[0].length; j++) {
                 int min = Integer.MAX_VALUE;
@@ -28,6 +31,7 @@ public class Q18_Paint_House_Many_Colrs{
                 dp[i][j] = arr[i][j] + min;
             }
         }
+        //again here we are using same approach to find min in last column that will become our ans
         int min = Integer.MAX_VALUE;
         for (int i = 0; i < dp[0].length; i++) {
             if(dp[n-1][i] < min){
@@ -36,5 +40,7 @@ public class Q18_Paint_House_Many_Colrs{
         }
         System.out.println(min);
         scn.close();
+        //in this approach we are using for - for - for that means complexity will become n^3 which is not 
+        //good so in next approach will try to solve this problem by having n^2 time complexity
     }
 }
