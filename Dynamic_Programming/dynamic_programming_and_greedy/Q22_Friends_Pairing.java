12bd175ecdf9fa6ec6d66d1eee35d4c7c55a05a1
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-18 19:31:25 +0530

    initial commit

--- Dynamic_Programming/dynamic_programming_and_greedy/Q22_Friends_Pairing.java
+++ Dynamic_Programming/dynamic_programming_and_greedy/Q22_Friends_Pairing.java
@@ -3,6 +3,17 @@ import java.util.Scanner;
 public class Q22_Friends_Pairing {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i < dp.length; i++) {
+            dp[i] = dp[i-1] + (n-1)*dp[i-2];
+        }
+        System.out.println(dp[n]);
+
         scn.close();
     }
 }
