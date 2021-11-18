986da65a360805754afb671c85e7d14a6ae2ada7
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-18 15:22:05 +0530

    intial commit

--- Dynamic_Programming/dynamic_programming_and_greedy/Q19_Paint_Fence.java
+++ Dynamic_Programming/dynamic_programming_and_greedy/Q19_Paint_Fence.java
@@ -3,6 +3,20 @@ import java.util.Scanner;
 public class Q19_Paint_Fence {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int k = scn.nextInt();
+
+        int same = k * 1;
+        int diff = k * (k-1);
+        int total = same + diff;
+
+        for (int i = 3; i <= n; i++) {
+            same = diff * 1;
+            diff = total * (k-1);
+            total = same + diff;
+        }
+
+        System.out.println(total);
         scn.close();
     }
 }
