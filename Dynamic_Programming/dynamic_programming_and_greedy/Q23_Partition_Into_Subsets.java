b5314c6b9a218e270fcdb8372c9c96454f9ab31b
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-18 19:39:38 +0530

    initial commit

---
+++ Dynamic_Programming/dynamic_programming_and_greedy/Q23_Partition_Into_Subsets.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Q23_Partition_Into_Subsets {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.close();
+    }
+}
