ef6b9fc918c7372fe7348f626bba3fec483e9d95
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-18 17:17:27 +0530

    initial commit

--- Dynamic_Programming/dynamic_programming_and_greedy/Q21_Tiling_With_M_by_1_Tiles.java
+++ Dynamic_Programming/dynamic_programming_and_greedy/Q21_Tiling_With_M_by_1_Tiles.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
-
+//In this question we have n*m size floor and m*1 infinite tiles
+//till i less than m we can arrange the m*1 tile in one way that it horizontal
+//when i and m equal we can arrange the m*1 tile in two ways one horizonatlly or vertically
+//else we can arrange using recursion we arrange first tile in vertical then rest will be calculate
+//from dp[i-1] and if we arrange first tile horizontally then we can arrange in dp[n-m]
+//at end add both
 public class Q21_Tiling_With_M_by_1_Tiles {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
