2e0645cbc95f9a0b28a23005505d37d1481babf4
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-18 15:34:07 +0530

    initial commit

---
+++ Dynamic_Programming/dynamic_programming_and_greedy/Q20_Tiling_With_2_by_1_Tiles.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Q20_Tiling_With_2_by_1_Tiles {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        scn.close();
+    }
+}
