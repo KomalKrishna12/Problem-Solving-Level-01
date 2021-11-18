94d381677a1ecafb019b8d7398a0584dd25ce792
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-18 14:37:18 +0530

    initial commit

---
+++ Dynamic_Programming/dynamic_programming_and_greedy/Q19_Paint_Fence.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Q19_Paint_Fence {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        scn.close();
+    }
+}
