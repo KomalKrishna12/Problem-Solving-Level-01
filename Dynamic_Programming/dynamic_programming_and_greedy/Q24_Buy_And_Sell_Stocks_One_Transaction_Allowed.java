09224cbd6763d168d2e2334c0f3539e3f16626a2
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-19 10:16:59 +0530

    initial commit

---
+++ Dynamic_Programming/dynamic_programming_and_greedy/Q24_Buy_And_Sell_Stocks_One_Transaction_Allowed.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Q24_Buy_And_Sell_Stocks_One_Transaction_Allowed{
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        scn.close();
+    }
+}
