6ddcd38b2acd7020855ee7395b4f6e4bff50a40e
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-19 20:22:59 +0530

    initial commit

--- Dynamic_Programming/dynamic_programming_and_greedy/Q25_Buy_And_Sell_Stocks_Infinite_Transaction_Allowed.java
+++ Dynamic_Programming/dynamic_programming_and_greedy/Q25_Buy_And_Sell_Stocks_Infinite_Transaction_Allowed.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
-
+//In this question we have n no of days 
+//In each days there is price of stocks
+//infinite no of transaction is allowed
 public class Q25_Buy_And_Sell_Stocks_Infinite_Transaction_Allowed {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
@@ -8,6 +10,11 @@ public class Q25_Buy_And_Sell_Stocks_Infinite_Transaction_Allowed {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = scn.nextInt();
         }
+        //at start set bp(buy point) and sp(sell point) at 0th index
+        //at start profit will be zero
+        //when next point price is greater than last then increse sell point
+        //when next point price is less than last that time calculate profit and set sp and bp at i
+        //for last we have to calculate sp again beacuse there is no further down
         int bp = 0;
         int sp = 0;
         int profit = 0;
