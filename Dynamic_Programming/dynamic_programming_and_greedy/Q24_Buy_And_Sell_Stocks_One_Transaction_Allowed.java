7b8d0e8646e67847f63163d64e05b7e5a3f434b7
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-19 10:41:17 +0530

    initial commit

--- Dynamic_Programming/dynamic_programming_and_greedy/Q24_Buy_And_Sell_Stocks_One_Transaction_Allowed.java
+++ Dynamic_Programming/dynamic_programming_and_greedy/Q24_Buy_And_Sell_Stocks_One_Transaction_Allowed.java
@@ -3,6 +3,29 @@ import java.util.Scanner;
 public class Q24_Buy_And_Sell_Stocks_One_Transaction_Allowed{
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int[] prices = new int[n];
+        for (int i = 0; i < prices.length; i++) {
+            prices[i] = scn.nextInt();
+        }
+
+        int leastInAllPrevDays = Integer.MAX_VALUE;
+        int overallProfit = 0;
+        int profitIfSoldToday = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if(leastInAllPrevDays > prices[i]){
+                leastInAllPrevDays = prices[i];
+            }
+            profitIfSoldToday = prices[i] - leastInAllPrevDays;
+            if(overallProfit < profitIfSoldToday){
+                overallProfit = profitIfSoldToday;
+            }
+        }
+
+        System.out.println(overallProfit);
+
         scn.close();
     }
 }
