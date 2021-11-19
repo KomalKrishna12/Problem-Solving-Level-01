20bec9e59072ce230af7b55196487c0a86a78597
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-19 10:47:07 +0530

    initial commit

--- Dynamic_Programming/dynamic_programming_and_greedy/Q24_Buy_And_Sell_Stocks_One_Transaction_Allowed.java
+++ Dynamic_Programming/dynamic_programming_and_greedy/Q24_Buy_And_Sell_Stocks_One_Transaction_Allowed.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
-
+//In this question only one transaction is allowed
+//n is no of days
+//and prices array contains prices for n days
+//we create one var which count least price from previous days so that to calculate profit we can 
+//subtract that least from the price of that particular day which we store in other var profitifsoldtoday
+//one other variable is overallprofit which will be the ans
 public class Q24_Buy_And_Sell_Stocks_One_Transaction_Allowed{
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
