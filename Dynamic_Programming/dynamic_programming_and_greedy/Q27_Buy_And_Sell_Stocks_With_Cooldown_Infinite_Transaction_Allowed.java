e6844c83fd5cb5eb835e98174e64e69ce3bbdbe0
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-20 12:48:30 +0530

    initial commit

---
+++ Dynamic_Programming/dynamic_programming_and_greedy/Q27_Buy_And_Sell_Stocks_With_Cooldown_Infinite_Transaction_Allowed.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Q27_Buy_And_Sell_Stocks_With_Cooldown_Infinite_Transaction_Allowed {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        scn.close();
+    }
+}
