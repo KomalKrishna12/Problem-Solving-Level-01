216f1bb4a2d48fa4293a6cb19ede0973992ddaa0
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-21 12:32:39 +0530

    initial commit

--- Dynamic_Programming/dynamic_programming_and_greedy/Q29_Buy_And_Sell_Stocks_K_Transactions_Allowed.java
+++ Dynamic_Programming/dynamic_programming_and_greedy/Q29_Buy_And_Sell_Stocks_K_Transactions_Allowed.java
@@ -1,5 +1,13 @@
 import java.util.Scanner;
-
+//In this ques we have n days
+//k transactions
+//we create a dp with row equal to k+1 and column equal to n
+//loop will start with t = 1 that means we can calculate max profit for atleast one transaction
+//and d = 1 beacause at d=0 only one stock is there so we buy ans sell same day so profit will become zero
+//to caluclate max profit 
+//create variable max store value at dp[t][d-1] that means profit where transaction completed in prev day
+//now compare that max with each dp[t-1][pd] pd-(0,d) and add particular share price
+//and replace max with these values
 public class Q29_Buy_And_Sell_Stocks_K_Transactions_Allowed {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
