2dc69226060615076664e0e348b281439782a30b
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-21 13:16:28 +0530

    initial commit

--- Dynamic_Programming/dynamic_programming_and_greedy/Q29_Buy_And_Sell_Stocks_K_Transactions_Allowed_Second_Aproach.java
+++ Dynamic_Programming/dynamic_programming_and_greedy/Q29_Buy_And_Sell_Stocks_K_Transactions_Allowed_Second_Aproach.java
@@ -1,5 +1,12 @@
 import java.util.Scanner;
-
+//this is the second approach to calculate max profit for k transaction
+//in last approach we used three for loops so time complexity will become n^3 that is not good code
+//so in this qpproach we calculate max profit in two for loops
+//we craete a max variable intialize it with Integer.MIN_VALUE then calculate dp[t-1][d-1] - arr[d-1]
+//compare this with max if this is greater than update max so using this max we can caluculate
+//max value till t-1 and d-1 which we did using third loop in last approach so now we simply add arr[d]
+//into max and compare it with dp[t][d-1] if greater than set dp[t][d] with max + arr[d] otherwise set 
+//last index value that is dp[t][d-1] 
 public class Q29_Buy_And_Sell_Stocks_K_Transactions_Allowed_Second_Aproach {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
