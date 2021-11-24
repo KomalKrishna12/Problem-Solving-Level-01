55a1c49c550883065022809f6b7965a3a93b596d
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-24 19:16:51 +0530

    initial commit

--- Stack_and_Queue/Q8_Infix_Evaluation.java
+++ Stack_and_Queue/Q8_Infix_Evaluation.java
@@ -1,10 +1,37 @@
 import java.util.Scanner;
 import java.util.Stack;
-
+//infix expression is a wxpression which human can understand
+//we have given an expression we have to solve them
+//we can use 4 operator only : '+', '-', '*', '/'
+//we have given single digit no
+//'*' and '/' having same precedence
+//'+' and '-' having same precedence
+//suppose '*' and '/' are together then solve from left to right
+//for eg : 2 + 6 * 4 / 8 - 3
+//   sol : 2 + 24 / 8 - 3
+//       : 2 + 3 - 3
+//       : 5 - 3
+//       : 2
+//so we use stack to solve this expression
+//create two array operator and operand
+//operand will store Integer like digit
+//operator will store charcters like operator
+//start loop from i -> 0 to exp.length()
+//get char by char using exp.charAt(i)
+//if char is '(' push in operator stack
+//if char is digit push in operand stack
+//if char is ')' then solve it till we get ')' at peek of operator stack
+//to solve first pop v2 from operand array then v1 becoz v1 push before v2 
+//then pop operator then calculate and store value into operand stack and at end pop '('
+//if char == '+' || '-' || '*' || '/'
+//solve then operand and operator array if operator stack contain any higher precedence operator or equal
+//precedence operator also
+//after loop ended there is chance of having some data in stack so solve stack till operator stack 
+//become empty and at end display operand stack that contain the ans of expressin 
 public class Q8_Infix_Evaluation {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        String exp = scn.next();
+        String exp = scn.nextLine();
         scn.close();
         Stack<Integer> operands = new Stack<>();
         Stack<Character> operators = new Stack<>();
@@ -14,10 +41,11 @@ public class Q8_Infix_Evaluation {
                 operators.push(ch);
             }
             else if(Character.isDigit(ch)){
-                operands.push(ch - '0');
+                operands.push(ch - '0');//converting char into int ascii of '0' is 43 suppose ch = '5'
+                //its ascii is 48 so digit will be 48 - 43 = 5
             }
             else if(ch == ')'){
-                while(operators.peek() != '('){
+                while(operators.size() > 0 && operators.peek() != '('){
                     int v2 = operands.pop();
                     int v1 = operands.pop();
                     char chr = operators.pop();
@@ -48,6 +76,9 @@ public class Q8_Infix_Evaluation {
         System.out.println(operands.pop());
     }
 
+    //here we can check the precedence of operator
+    //'+' and '-' having same precedence but having less precedence than '*' and '/'
+    //'*' and '/' having same precedence
     public static int precedence(char ch) {
         if (ch == '+') {
             return 1;
@@ -60,6 +91,7 @@ public class Q8_Infix_Evaluation {
         }
     }
 
+    //here we can calculate the expression for v1, v2 and operator and return the calculated value
     public static int operation(int v1, int v2, char ch) {
         if (ch == '+') {
             return v1 + v2;
