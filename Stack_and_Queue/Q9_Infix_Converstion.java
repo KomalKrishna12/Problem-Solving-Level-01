4bf7ce4738674465744e910ba59fa01c3ed6c3ae
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-24 21:43:02 +0530

    initial commit

--- Stack_and_Queue/Q9_Infix_Converstion.java
+++ Stack_and_Queue/Q9_Infix_Converstion.java
@@ -1,6 +1,19 @@
 import java.util.Scanner;
 import java.util.Stack;
-
+//In this question we have given an infix expression
+//we have to convert into into postfix expression and prefix expression
+//for eg :
+// infix : a + b
+// postfix : ab+ (v1)(v2)(opr)
+// prefix : +ab (opr)(v1)(v2)
+//so to convert this we use infix evaluation
+//create post and pre two string stack
+//one opr integer stack
+//when ch == '(' push ch into opr stack
+//when ch == ['0','9'] || ['a','z'] || ['A','Z'] push into both post and pre stack
+//when ch == ')' calculate postfix and prefix untill opr.size()>0 && opr.peek()!='('
+//when ch == '+' || '-' || '*' || '/' calculate postfix and prefix till opr.size()>0 && opr.peek()!='('
+//&& pre(ch) <= pre(opr.peek())
 public class Q9_Infix_Converstion {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
