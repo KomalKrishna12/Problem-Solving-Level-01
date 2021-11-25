4cfe3f48e034d70a7c2f25d314fc4362c4da72da
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-25 09:41:51 +0530

    initial commit

---
+++ Stack_and_Queue/Q10_Postfix_Evaluation_And_Conversion.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Q10_Postfix_Evaluation_And_Conversion {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String exp = scn.nextLine();
+        scn.close();
+    }
+}
