488500dc522f43deeb85d7b22ef40569f3c7b127
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-25 19:31:57 +0530

    initial commit

--- Stack_and_Queue/Q14_Smallest_No_Following_Pattern.java
+++ Stack_and_Queue/Q14_Smallest_No_Following_Pattern.java
@@ -1,7 +1,16 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Stack;
-
+//In this question we have to print smallest num
+//eg : d : 21,   i : 12
+//    dd : 321, ii : 123
+//   did : 2143
+//so we create a stack and a variable num initialize num with 1
+//create a for loop from 0 to str.length()
+//now take each char one by one
+//if char == 'd' then push num into stack and num++
+//if char == 'i' then push num and num++ and pop each value till st.size()>0
+//after loop push last element and print all element
 public class Q14_Smallest_No_Following_Pattern {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
