fc90c15479c436259084eb0fee5bc978ece56a9a
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-25 12:53:13 +0530

    initial commit

--- Stack_and_Queue/Q10_Postfix_Evaluation_And_Conversion.java
+++ Stack_and_Queue/Q10_Postfix_Evaluation_And_Conversion.java
@@ -1,6 +1,19 @@
 import java.util.Scanner;
 import java.util.Stack;
-
+//in this question we have given a postfix expression
+//we have to solve the expression
+//and print its into infix expression and prefix expression
+//for eg : 
+//postfix exp  : ab+
+//infix exp   : (a+b)
+//prefix exp : +ab
+//so we create three stack one Integer stack to store value of expression
+//and rest two for infix and postfix expression
+//start loop from i = 0 to exp.length()
+//if ch == any digit then simply push it into all three array
+//if ch == '+' || '-' || '*' || '/' then calculate v1 and v2 and then val and then push into corresponding 
+//stack.
+//at end dislay all 
 public class Q10_Postfix_Evaluation_And_Conversion {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
@@ -14,21 +27,24 @@ public class Q10_Postfix_Evaluation_And_Conversion {
         for (int i = 0; i < exp.length(); i++) {
             char ch = exp.charAt(i);
             if(Character.isDigit(ch)){
-                value.push(ch - '0');
-                infix.push(ch + "");
-                prefix.push(ch + "");
+                value.push(ch - '0'); //converting char to int
+                infix.push(ch + "");  //converting char to String
+                prefix.push(ch + ""); //converting char to String
             }
             else if(ch == '+' || ch == '-' || ch == '*' || ch == '/'){
+                //calculate value using operation function
                 int v2 = value.pop();
                 int v1 = value.pop();
                 int val = operation(v1, v2, ch);
                 value.push(val);
 
+                //calculate string using infix concept : val1 opr val2
                 String infv2 = infix.pop();
                 String infv1 = infix.pop();
                 String infv = "(" + infv1 + ch + infv2 +")";
                 infix.push(infv);
 
+                //calculate string using prefix concept : opr val1 val2 
                 String prev2 = prefix.pop();
                 String prev1 = prefix.pop();
                 String prev = ch + prev1 + prev2;
--- Stack_and_Queue/Q11_Prefix_Evaluation_And_Conversion.java
+++ Stack_and_Queue/Q11_Prefix_Evaluation_And_Conversion.java
@@ -1,6 +1,19 @@
 import java.util.Scanner;
 import java.util.Stack;
-
+//in this question we have given a prefix expression
+//we have to solve the expression
+//and print its into infix expression and postfix expression
+//for eg : 
+//prefix exp  : +ab
+//infix exp   : (a+b)
+//postfix exp : ab+
+//so we create three stack one Integer stack to store value of expression
+//and rest two for infix and postfix expression
+//start loop from i = 0 to exp.length()
+//if ch == any digit then simply push it into all three array
+//if ch == '+' || '-' || '*' || '/' then calculate v1 and v2 and then val and then push into corresponding 
+//stack.
+//at end dislay all 
 public class Q11_Prefix_Evaluation_And_Conversion {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
@@ -14,16 +27,19 @@ public class Q11_Prefix_Evaluation_And_Conversion {
         for (int i = exp.length()-1; i >= 0; i--) {
             char ch = exp.charAt(i);
             if(ch == '+' || ch == '-' || ch == '*' || ch == '/'){
+                //calculate value using operation function
                 int v1 = value.pop();
                 int v2 = value.pop();
                 int v = operation(v1, v2, ch);
                 value.push(v);
 
+                //calculate string using infix concept : val1 opr val2
                 String iv1 = infix.pop();
                 String iv2 = infix.pop();
                 String iv = "(" + iv1 + ch + iv2 +")";
                 infix.push(iv);
 
+                //calculate string using postfix concept : val1 val2 opr
                 String pv1 = postfix.pop();
                 String pv2 = postfix.pop();
                 String pv = pv1 + pv2 + ch;
