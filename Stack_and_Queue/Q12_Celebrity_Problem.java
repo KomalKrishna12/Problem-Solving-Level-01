6885d6b30bc8b81450783ed5dbed094b23c738e1
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-25 13:36:50 +0530

    initial commit

--- Stack_and_Queue/Q12_Celebrity_Problem.java
+++ Stack_and_Queue/Q12_Celebrity_Problem.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Q12_Celebrity_Problem {
     public static void main(String[] args) {
-        
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = scn.nextInt();
+            }
+        }
+        scn.close();
+        findCelebrity(arr);
     }
+
+    private static void findCelebrity(int[][] arr) {
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            st.push(i);
+        }
+        int potentialCeleb = st.pop();
+        for (int i = 0; i < arr.length; i++) {
+            if(i != potentialCeleb)
+            {
+                if(arr[i][potentialCeleb] == 0 || arr[potentialCeleb][i] == 1){
+                    System.out.println("none");
+                    return;
+                }
+            }
+        }
+        System.out.println(potentialCeleb);
+    }
+
 }
