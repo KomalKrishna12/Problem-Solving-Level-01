531fcbd861960778657322738972939dc908ccc0
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-26 12:40:42 +0530

    initial commit

---
+++ Stack_and_Queue/Q15_Queue_Basic_Functions.java
@@ -0,0 +1,18 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class Q15_Queue_Basic_Functions{
+    public static void main(String[] args) {
+        Queue<Integer> queue = new ArrayDeque<>();
+
+        queue.add(10);
+        queue.add(20);
+        System.out.println(queue);
+
+        System.out.println(queue.peek());
+        System.out.println(queue);
+
+        System.out.println(queue.remove());
+        System.out.println(queue);
+    }
+}
