4a61f9354b9b99c57cb80323a11966aa38857a4d
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-26 21:32:22 +0530

    initial commit

---
+++ Stack_and_Queue/Q21_Stack_To_Queue_Adapter_Add_Effecient.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+//in this ques we have two stack that means we have LIFO structure and we want to adapt
+//FIFO which is of Queue
+//and add effeceint means add has to be in o(1)
+//so work on pop and top
+public class Q21_Stack_To_Queue_Adapter_Add_Effecient {
+    public static class StackToQueueAdapter {
+        Stack<Integer> mainS;
+        Stack<Integer> helperS;
+
+        public StackToQueueAdapter() {
+            mainS = new Stack<>();
+            helperS = new Stack<>();
+        }
+
+        int size() {
+            return mainS.size();
+        }
+
+        void add(int val) {
+            mainS.add(val);
+        }
+
+        int remove() {
+            while(mainS.size() > 1){
+                helperS.push(mainS.pop());
+            }
+            int val = mainS.pop();
+            while (helperS.size() > 0) {
+                mainS.push(helperS.pop());
+            }
+            return val;
+        }
+
+        int peek() {
+            while(mainS.size() > 1){
+                helperS.push(mainS.pop());
+            }
+            int val = mainS.pop();
+            helperS.push(val);
+            while (helperS.size() > 0) {
+                mainS.push(helperS.pop());
+            }
+            return val;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StackToQueueAdapter qu = new StackToQueueAdapter();
+
+        String str = br.readLine();
+        while (str.equals("quit") == false) {
+            if (str.startsWith("add")) {
+                int val = Integer.parseInt(str.split(" ")[1]);
+                qu.add(val);
+            } else if (str.startsWith("remove")) {
+                int val = qu.remove();
+                if (val != -1) {
+                    System.out.println(val);
+                }
+            } else if (str.startsWith("peek")) {
+                int val = qu.peek();
+                if (val != -1) {
+                    System.out.println(val);
+                }
+            } else if (str.startsWith("size")) {
+                System.out.println(qu.size());
+            }
+            str = br.readLine();
+        }
+    }
+}
