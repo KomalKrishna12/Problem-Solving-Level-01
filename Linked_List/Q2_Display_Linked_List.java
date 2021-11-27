d84bde03384aa8af5942ad6e18405084943f1e6a
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-27 11:05:48 +0530

    initial commit

---
+++ Linked_List/Q2_Display_Linked_List.java
@@ -0,0 +1,62 @@
+import java.io.*;
+public class Q2_Display_Linked_List {
+    public static class Node{
+        int data;
+        Node next;
+    }
+    public static class LinkedList{
+        Node head;
+        Node tail;
+        int size;
+        public LinkedList(){
+            head = null;
+            tail = null;
+            size = 0;
+        }
+        public void addLast(int val){
+            Node temp = new Node();
+            temp.data = val;
+            temp.next = null;
+            if(size == 0){
+                head = tail = temp;
+            }
+            else{
+                tail.next = temp;
+                tail = temp;
+            }
+            size++;
+        }
+        
+        public int size(){
+            return size;
+        }
+
+        public void display(){
+            //we create a node temp assign it with head and run the loop till temp beacome run
+            //and print temp.data and assign temp to its next value
+            Node temp = head;
+            while(temp != null){
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        LinkedList list = new LinkedList();
+    
+        String str = br.readLine();
+        while(str.equals("quit") == false){
+          if(str.startsWith("addLast")){
+            int val = Integer.parseInt(str.split(" ")[1]);
+            list.addLast(val);
+          } else if(str.startsWith("size")){
+            System.out.println(list.size());
+          } else if(str.startsWith("display")){
+            list.display();
+          }
+          str = br.readLine();
+        }
+      }
+}
