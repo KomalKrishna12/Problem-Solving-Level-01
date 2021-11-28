0f0889cb9e4530c4a4a4faf829f75a723b36efa4
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-28 17:39:57 +0530

    initial commit

---
+++ Linked_List/Q19_Segregate_Even_Odd_Node_Linked_List.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Q19_Segregate_Even_Odd_Node_Linked_List {
+    public static class ListNode {
+        int val = 0;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static ListNode segregateEvenOdd(ListNode head) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        scn.close();
+        ListNode dummy = new ListNode(-1);
+        ListNode prev = dummy;
+        while (n-- > 0) {
+            prev.next = new ListNode(scn.nextInt());
+            prev = prev.next;
+        }
+
+        ListNode head = segregateEvenOdd(dummy.next);
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+    }
+}
