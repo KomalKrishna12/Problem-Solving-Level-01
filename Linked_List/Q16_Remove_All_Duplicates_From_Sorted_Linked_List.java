bb54c3ffc7297f2c9b00e83966f22f0ecadb66c8
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-28 14:36:32 +0530

    initial commit

---
+++ Linked_List/Q16_Remove_All_Duplicates_From_Sorted_Linked_List.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+// i/p : 1->1->1->4->5->6->6->7->8->9->9->9->null
+// o/p : 4->5->7->8->null
+public class Q16_Remove_All_Duplicates_From_Sorted_Linked_List {
+    public static Scanner scn = new Scanner(System.in);
+
+    public static class ListNode {
+        int val = 0;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static ListNode removeDuplicates(ListNode head) {
+        return null;
+    }
+
+    // InFput_code===================================================
+
+    public static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val + " ");
+            node = node.next;
+        }
+    }
+
+    public static ListNode makeList(int n) {
+        ListNode dummy = new ListNode(-1);
+        ListNode prev = dummy;
+        while (n-- > 0) {
+            prev.next = new ListNode(scn.nextInt());
+            prev = prev.next;
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = makeList(scn.nextInt());
+
+        ListNode ans = removeDuplicates(head);
+        printList(ans);
+    }
+}
