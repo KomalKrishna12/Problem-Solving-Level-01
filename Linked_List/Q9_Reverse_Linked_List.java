0cf6f82e069c008579e2693b3184212505652f6b
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-29 09:53:54 +0530

    initial commit

---
+++ Linked_List/Q9_Reverse_Linked_List.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Q9_Reverse_Linked_List {
+    public static class ListNode {
+        int val = 0;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static ListNode reverse(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode forw = null;
+        while(curr != null){
+            forw = curr.next;
+            
+            curr.next = prev;
+            prev = curr;
+            curr = forw;
+        }
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        ListNode dummy = new ListNode(-1);
+        ListNode prev = dummy;
+        while (n-- > 0) {
+            prev.next = new ListNode(scn.nextInt());
+            prev = prev.next;
+        }
+        scn.close();
+        ListNode head = reverse(dummy.next);
+        while (head != null) {
+            System.out.print(head.val + " ");
+head = head.next;
+        }
+    }
+}
