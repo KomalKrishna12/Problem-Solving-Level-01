07d7cfcd0962b3bad6877e2a14e821af18ecd583
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-29 12:36:19 +0530

    initial commit

---
+++ Linked_List/Q23_Reverse_Node_In_Linked_List_In_K_Group_Order.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Q23_Reverse_Node_In_Linked_List_In_K_Group_Order {
+    public static Scanner scn = new Scanner(System.in);
+
+    public static class ListNode {
+        int val = 0;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static ListNode reverseInKGroup(ListNode head, int k) {
+        return null;
+    }
+
+    public static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val + " ");
+            node = node.next;
+        }
+    }
+
+    public static ListNode createList(int n) {
+        ListNode dummy = new ListNode(-1);
+        ListNode prev = dummy;
+        while (n-- > 0) {
+            prev.next = new ListNode(scn.nextInt());
+            prev = prev.next;
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        int n = scn.nextInt();
+        ListNode h1 = createList(n);
+
+        int k = scn.nextInt();
+        h1 = reverseInKGroup(h1, k);
+        printList(h1);
+    }
+}
