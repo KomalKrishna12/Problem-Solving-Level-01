a0e2e201c53c1cc3135f3b18b6f70b89f33cd7dd
KomalKrishna12 <komalkrishna016@gmail.com>
2021-11-30 10:30:56 +0530

    initial commit

--- Linked_List/Q28_Unfold_Linked_List.java
+++ Linked_List/Q28_Unfold_Linked_List.java
@@ -1,4 +1,8 @@
 import java.util.*;
+// i/p : 1->7->2->6->3->5->4->null
+// o/p : 1->2->3->4->5->6->7->null
+// in fold linked list we have given o/p as i/p to fold the list
+// and here we want to unfold it again to its prev stage
 public class Q28_Unfold_Linked_List {
     public static class ListNode {
         int val = 0;
@@ -10,7 +14,7 @@ public class Q28_Unfold_Linked_List {
     }
     
     public static ListNode reverse(ListNode head){
-        ListNode prev = null;
+        ListNode prev = null; // it'll become our new head for reverse list
         ListNode curr = head;
         ListNode forw = null;
         while(curr != null){
@@ -22,11 +26,18 @@ public class Q28_Unfold_Linked_List {
         return prev;
     }
 
+    // create two node first head(fh) and second head(sh)
+    // fh point to head and sh point to head.next
+    // ft is tail of fh and st is tail of sh 
+    // f is for storing next of sh
     public static void unfold(ListNode head) {
         if(head == null || head.next == null) return;
         ListNode fh = head, sh = head.next;
         ListNode ft = fh, st = sh;
         ListNode f = null;
+        // st is the smallest list so in odd no of node st go aat end
+        // but in even no of cases st go to null
+        // so we put condtion for st only
         while(st != null && st.next != null){
             f = st.next; //backup
             
@@ -36,8 +47,14 @@ public class Q28_Unfold_Linked_List {
             ft = ft.next;
             st = st.next;
         }
+        // in even no of nodes ft is still pointing to next node 
+        // so after loop point ft.next to null
         ft.next = null;
+
+        // now reverse the sh
         sh = reverse(sh);
+
+        // link both list
         ft.next = sh;
     }
 
