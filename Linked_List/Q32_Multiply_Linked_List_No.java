a99066faf15d2797725ccaa70c893f8179793222
KomalKrishna12 <komalkrishna016@gmail.com>
2021-12-01 19:26:04 +0530

    initial commit

---
+++ Linked_List/Q32_Multiply_Linked_List_No.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Q32_Multiply_Linked_List_No {
+    public static Scanner scn = new Scanner(System.in);
+
+    public static class ListNode {
+        int val = 0;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static ListNode makeList(int n) {
+        ListNode dummy = new ListNode(-1);
+        ListNode prev = dummy;
+        while (n-- > 0) {
+            prev.next = new ListNode(scn.nextInt());
+            prev = prev.next;
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode head1 = makeList(scn.nextInt());
+        ListNode head2 = makeList(scn.nextInt());
+
+        printList(head1);
+        printList(head2);
+    }
+}
