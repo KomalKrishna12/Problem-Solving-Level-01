f2e67323fe3e8203e4b7750c4b4fafadfab0bb66
KomalKrishna12 <komalkrishna016@gmail.com>
2021-12-05 21:55:10 +0530

    initial commit

--- Linked_List/Q43_Add_First_In_Doubly_Linked_List.java
+++ Linked_List/Q43_Add_First_In_Doubly_Linked_List.java
@@ -31,18 +31,28 @@ public class Q43_Add_First_In_Doubly_Linked_List {
     
           return sb.toString();
         }
-        private void addFirstNode(Node node){
+        
+        // if size is 0 then that node is the only one node of list
+        // so head and tail both point to that node
+        public void addFirstNode(Node node){
             if(this.size == 0){
-                this.head = this.tail = node;
+                this.head = node;
+                this.tail = node;
             }
             else{
+                // if size is not zero that node.next will point to head
+                // head.prev point to node
+                // and that new node becomes our head
                 node.next = this.head;
                 this.head.prev = node;
                 this.head = node;
             }
+            // increase size because we're adding a node in list
             this.size++;
         }
     
+        // we create a node wihich contain val as data
+        // and call addFirstNode(node) 
         public void addFirst(int val) {
             Node node = new Node(val);
             addFirstNode(node);
