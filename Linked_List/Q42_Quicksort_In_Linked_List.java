154f9bccd281ed705c53c2184c6b39b9fcdab9ac
KomalKrishna12 <komalkrishna016@gmail.com>
2021-12-05 16:57:28 +0530

    initial commit

--- Linked_List/Q42_Quicksort_In_Linked_List.java
+++ Linked_List/Q42_Quicksort_In_Linked_List.java
@@ -71,6 +71,12 @@ public class Q42_Quicksort_In_Linked_List{
     // and it'll return 1 as head and 8 as tail
     public static ListNode[] mergeseg(ListNode[] leftseg, ListNode pivot, ListNode[] rightseg) {
         ListNode head = null, tail = null;
+
+        // if leftseg is not null then leftseg[0] will be the head and leftseg[1] will be the tail
+        // same rightseg[0] head and rightseg[1] tail
+        // now connect tail of left seg(leftseg[1]) with pivot
+        // pivot with head of right seg(rightseg[0])
+        // our head become rightseg[0] and tail leftseg[1]
         if(leftseg[0] != null && rightseg[0] != null){
             leftseg[1].next = pivot;
             pivot.next = rightseg[0];
@@ -78,28 +84,50 @@ public class Q42_Quicksort_In_Linked_List{
             tail = rightseg[1];
         }
         else if(leftseg[0] != null){
+            // leftseg is not null but right seg is null
+            // so connect tail of leftseg(leftseg[1]) with pivot
+            // head will be leftseg[0] and pivot will be our tail
             leftseg[1].next = pivot;
             head = leftseg[0];
             tail = pivot;
         }
         else if(rightseg[0] != null){
+            // if leftseg is null but right seg is not null so
+            // pivot become head and pivot wil connet to head of rightseg (righseg[0])
+            // so tail will be righseg[1]
             head = pivot;
             pivot.next = rightseg[0];
             tail = rightseg[1];
         }
         else{
+            // if both leftseg and rightseg is null then pivot will be our head and tail
             head = tail = pivot;
         }
+        // now return head and tail as an array element
         return new ListNode[]{head, tail};
     }
+
+    // this is the function which return head and tail after calculating
+    // pivot, leftseg, rightseg and after merging all three
     public static ListNode[] quickSort_(ListNode head) {
+        // if head is null or only head is present in list then that head will 
+        // be our head as well as tail
         if(head == null || head.next == null){
             return new ListNode[]{head, head};
         }
+        // calculate len using length(head) function
         int len = length(head);
+        // pivot will be the mid of head
         int pivot = len / 2;
 
+        // now call segregate(head,pivot) function for mid elemnet as pivot
+        // it will return a ListNode[] which contain three elements
+        // ListNode[0] = head of small list
+        // ListNode[1] = pivot node
+        // ListNode[2] = head of large list
         ListNode[] segreagate = segregateNode(head, pivot);
+        // now we want head as well as tell so we use recursion and call itself for small
+        // and large head and tail
         ListNode[] leftseg = quickSort_(segreagate[0]);
         ListNode[] rightseg = quickSort_(segreagate[2]);
 
@@ -108,6 +136,7 @@ public class Q42_Quicksort_In_Linked_List{
     }
 
     public static ListNode quickSort(ListNode head) {
+        // but we want to return only head so here we pass as index 0
         return quickSort_(head)[0];
     }
 
