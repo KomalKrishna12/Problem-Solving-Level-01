00633aa509ad39b22e667c306ace76600720d162
KomalKrishna12 <komalkrishna016@gmail.com>
2021-12-08 20:45:53 +0530

    initial commit

--- Hashmap_and_Heap/Q1_Function_Of_Hashmap.java
+++ Hashmap_and_Heap/Q1_Function_Of_Hashmap.java
@@ -3,6 +3,8 @@ import java.util.HashMap;
 public class Q1_Function_Of_Hashmap{
     public static void main(String[] args) {
         HashMap<String, Integer> hm = new HashMap<>();
+
+        // put(key, value);
         // put() function is taking two parameter one is key and other is value
         // if that key is already existing then it'll update it's value otherwise it'll add the key for
         // that value
@@ -13,5 +15,6 @@ public class Q1_Function_Of_Hashmap{
 
         // "pakistan" key is already present so it'll update it's value
         hm.put("Pakistan", 22);
+        System.out.println(hm);
     }
 }
