bfa076034031d0481ed9f0e089d1d783ac0ac4a9
KomalKrishna12 <komalkrishna016@gmail.com>
2021-12-10 10:00:30 +0530

    priority queue

--- Hashmap_and_Heap/Q8_Sort_K_Sorted_Array.java
+++ Hashmap_and_Heap/Q8_Sort_K_Sorted_Array.java
@@ -1,6 +1,14 @@
 import java.util.PriorityQueue;
 import java.util.Scanner;
-
+// In this question we have given a k sorted array like partially sorted array
+// we're required to sort the array
+// n = 9
+// arr[] : 3 2 4 1 6 5 7 9 8 
+// k = 3
+// so we create a for loop for [0 to k] and add all in pq (priority queue object)
+// now again create a loop for [k + 1 to n)
+// print peek and add arr[k+1]
+// after loop create a while loop and print all element
 public class Q8_Sort_K_Sorted_Array {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
