c60417e2e7bfb4d4ca0c7163548c546875c3dc84
KomalKrishna12 <komalkrishna016@gmail.com>
2021-12-11 23:06:18 +0530

    size of generic tree

---
+++ Genric_Tree/Q2_Size_Of_Generic_Tree.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Stack;
+public class Q2_Size_Of_Generic_Tree {
+    private static class Node {
+        int data;
+        ArrayList<Node> children = new ArrayList<>();
+      }
+    
+      public static void display(Node node) {
+        String str = node.data + " -> ";
+        for (Node child : node.children) {
+          str += child.data + ", ";
+        }
+        str += ".";
+        System.out.println(str);
+    
+        for (Node child : node.children) {
+          display(child);
+        }
+      }
+    
+      public static Node construct(int[] arr) {
+        Node root = null;
+    
+        Stack<Node> st = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+          if (arr[i] == -1) {
+            st.pop();
+          } else {
+            Node t = new Node();
+            t.data = arr[i];
+    
+            if (st.size() > 0) {
+              st.peek().children.add(t);
+            } else {
+              root = t;
+            }
+    
+            st.push(t);
+          }
+        }
+    
+        return root;
+      }
+    
+      public static int size(Node node){
+          // if node is null that means size is 0
+          // create a variable sz initialize it with 0
+          // now run a for loop for node.children
+          // add size of child into sz
+          // at end add 1
+        if(node == null) return 0;
+        int sz = 0;
+        for(Node child : node.children){
+            sz += size(child);
+        }
+        sz += 1;
+        return sz;
+      }
+    
+      public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        String[] values = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+          arr[i] = Integer.parseInt(values[i]);
+        }
+    
+        Node root = construct(arr);
+        int sz = size(root);
+        System.out.println(sz);
+        // display(root);
+      }
+}
