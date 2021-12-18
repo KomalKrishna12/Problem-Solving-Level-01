041b68cd7522bd5b2041e7b3d38055ece5a7727a
KomalKrishna12 <komalkrishna016@gmail.com>
2021-12-18 22:20:03 +0530

    iterartive pre, in and post order traverse

--- Binary_Tree/Q7_Iterative_preorder_inorder_postorder_in_Binary_Tree.java
+++ Binary_Tree/Q7_Iterative_preorder_inorder_postorder_in_Binary_Tree.java
@@ -1,128 +1,130 @@
 import java.io.*;
 import java.util.*;
+
 public class Q7_Iterative_preorder_inorder_postorder_in_Binary_Tree {
     public static class Node {
         int data;
         Node left;
         Node right;
-    
+
         Node(int data, Node left, Node right) {
-          this.data = data;
-          this.left = left;
-          this.right = right;
+            this.data = data;
+            this.left = left;
+            this.right = right;
         }
-      }
-    
-      public static class Pair {
+    }
+
+    public static class Pair {
         Node node;
         int state;
-    
+
         Pair(Node node, int state) {
-          this.node = node;
-          this.state = state;
+            this.node = node;
+            this.state = state;
         }
-      }
-    
-      public static Node construct(Integer[] arr) {
+    }
+
+    public static Node construct(Integer[] arr) {
         Node root = new Node(arr[0], null, null);
         Pair rtp = new Pair(root, 1);
-    
+
         Stack<Pair> st = new Stack<>();
         st.push(rtp);
-    
+
         int idx = 0;
         while (st.size() > 0) {
-          Pair top = st.peek();
-          if (top.state == 1) {
-            idx++;
-            if (arr[idx] != null) {
-              top.node.left = new Node(arr[idx], null, null);
-              Pair lp = new Pair(top.node.left, 1);
-              st.push(lp);
+            Pair top = st.peek();
+            if (top.state == 1) {
+                idx++;
+                if (arr[idx] != null) {
+                    top.node.left = new Node(arr[idx], null, null);
+                    Pair lp = new Pair(top.node.left, 1);
+                    st.push(lp);
+                } else {
+                    top.node.left = null;
+                }
+
+                top.state++;
+            } else if (top.state == 2) {
+                idx++;
+                if (arr[idx] != null) {
+                    top.node.right = new Node(arr[idx], null, null);
+                    Pair rp = new Pair(top.node.right, 1);
+                    st.push(rp);
+                } else {
+                    top.node.right = null;
+                }
+
+                top.state++;
             } else {
-              top.node.left = null;
+                st.pop();
             }
-    
-            top.state++;
-          } else if (top.state == 2) {
-            idx++;
-            if (arr[idx] != null) {
-              top.node.right = new Node(arr[idx], null, null);
-              Pair rp = new Pair(top.node.right, 1);
-              st.push(rp);
-            } else {
-              top.node.right = null;
-            }
-    
-            top.state++;
-          } else {
-            st.pop();
-          }
         }
-    
+
         return root;
-      }
-    
-      public static void display(Node node) {
+    }
+
+    public static void display(Node node) {
         if (node == null) {
-          return;
+            return;
         }
-    
+
         String str = "";
         str += node.left == null ? "." : node.left.data + "";
         str += " <- " + node.data + " -> ";
         str += node.right == null ? "." : node.right.data + "";
         System.out.println(str);
-    
+
         display(node.left);
         display(node.right);
-      }
-    
-      public static void iterativePrePostInTraversal(Node node) {
-          Stack<Pair> st = new Stack<>();
-          Pair rootPair = new Pair(node, 1);
-          st.push(rootPair);
-          String pre = "", in = "",post = "";
-          while(st.size() > 0){
-              Pair top = st.peek();
-              if(top.state == 1){ // state : 1 -> preorder, state++, add left children
+    }
+
+    public static void iterativePrePostInTraversal(Node node) {
+        Stack<Pair> st = new Stack<>();
+        Pair rootPair = new Pair(node, 1);
+        st.push(rootPair);
+        String pre = "", in = "", post = "";
+        while (st.size() > 0) {
+            Pair top = st.peek();
+            if (top.state == 1) { // state : 1 -> preorder, state++, add left children
                 pre += top.node.data + " ";
                 top.state++;
-
-                Pair leftchild = new Pair(top.node.left, 1);
-                st.push(leftchild);
-              }
-              else if(top.state == 2){ // state : 2 -> inorder, state++, add right children
+                if (top.node.left != null) {
+                    Pair leftchild = new Pair(top.node.left, 1);
+                    st.push(leftchild);
+                }
+            } else if (top.state == 2) { // state : 2 -> inorder, state++, add right children
                 in += top.node.data + " ";
                 top.state++;
 
-                Pair rightchild = new Pair(top.node.right, 1);
-                st.push(rightchild);
-              }
-              else{ // state : 3 -> postorder, pop
+                if (top.node.right != null) {
+                    Pair rightchild = new Pair(top.node.right, 1);
+                    st.push(rightchild);
+                }
+            } else { // state : 3 -> postorder, pop
                 post += top.node.data + " ";
                 st.pop();
-              }
-          }
-          System.out.println(pre);
-          System.out.println(in);
-          System.out.println(post);
-      }
-    
-      public static void main(String[] args) throws Exception {
+            }
+        }
+        System.out.println(pre);
+        System.out.println(in);
+        System.out.println(post);
+    }
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         Integer[] arr = new Integer[n];
         String[] values = br.readLine().split(" ");
         for (int i = 0; i < n; i++) {
-          if (values[i].equals("n") == false) {
-            arr[i] = Integer.parseInt(values[i]);
-          } else {
-            arr[i] = null;
-          }
+            if (values[i].equals("n") == false) {
+                arr[i] = Integer.parseInt(values[i]);
+            } else {
+                arr[i] = null;
+            }
         }
-    
+
         Node root = construct(arr);
         iterativePrePostInTraversal(root);
-      }
+    }
 }
