136e9b1c4d428279b494ee457a0a40302b0d47b8
KomalKrishna12 <komalkrishna016@gmail.com>
2021-12-18 16:14:11 +0530

    binary tree

---
+++ Binary_Tree/Q1_Binary_Tree_Basic.java
@@ -0,0 +1,14 @@
+public class Q1_Binary_Tree_Basic{
+    public static class Node{
+        int data;
+        Node left, right;
+        Node(int data, Node left, Node right){
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
