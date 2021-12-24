b9650de1cc701d39962dfc0896e4ffc0b4ff222e
KomalKrishna12 <komalkrishna016@gmail.com>
2021-12-24 13:06:31 +0530

    selection sort

---
+++ Revision/Selection_sort.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Selection_sort {
+    public static void display(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void selectionSort(int[] arr) {
+        
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = scn.nextInt();
+        }
+        selectionSort(arr);
+        display(arr);
+        scn.close();
+    }
+}
