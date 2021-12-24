754ffc2b98718c2fcf78d9ead06aa741b99bff7a
KomalKrishna12 <komalkrishna016@gmail.com>
2021-12-24 13:14:15 +0530

    selection sort

--- Revision/Selection_sort.java
+++ Revision/Selection_sort.java
@@ -7,14 +7,37 @@ public class Selection_sort {
         }
         System.out.println();
     }
+
     public static void selectionSort(int[] arr) {
-        
+        for(int i = 0; i < arr.length - 1; i++){
+            int min = i;
+            for(int j = i + 1; j < arr.length; j++){
+                if(isSmaller(arr, j, min)){
+                    min = j;
+                }
+            }
+            swap(arr, i, min);
+        }
     }
+
+    public static boolean isSmaller(int[] arr, int i, int j) {
+        if (arr[i] < arr[j])
+            return true;
+        else
+            return false;
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         int[] arr = new int[n];
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             arr[i] = scn.nextInt();
         }
         selectionSort(arr);
