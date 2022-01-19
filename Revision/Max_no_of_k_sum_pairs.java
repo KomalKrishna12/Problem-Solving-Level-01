7d1f248d1fd85dd10f7e5eaa2f4c03c09144e6ae
KomalKrishna12 <komalkrishna016@gmail.com>
2022-01-19 20:16:46 +0530

    max no of k sum pairs

--- Revision/Max_no_of_k_sum_pairs.java
+++ Revision/Max_no_of_k_sum_pairs.java
@@ -1,5 +1,12 @@
 public class Max_no_of_k_sum_pairs {
+
+    public static int maxOperations(int[] nums, int k) {
+        int count = 0;
+        return count;
+    }
     public static void main(String[] args) {
-        
+        int[] nums = {1,2,3,4,5};
+        int k = 5;
+        System.out.println(maxOperations(nums, k));
     }
 }
