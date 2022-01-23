1d9cd9ce9956386fa1a7e6bed6f03f7655ba625e
KomalKrishna12 <komalkrishna016@gmail.com>
2022-01-23 22:21:13 +0530

    .

--- Revision/Reverse_string.java
+++ Revision/Reverse_string.java
@@ -1,6 +1,14 @@
 public class Reverse_string {
     public static void reverseString(char[] s) {
-        
+        int li = 0;
+        int ri = s.length - 1;
+        while(li < ri){
+            char temp = s[li];
+            s[li] = s[ri];
+            s[ri] = temp;
+            li++;
+            ri--;
+        }
     }
     public static void display(char[] s){
         for(char ch : s){
@@ -10,6 +18,8 @@ public class Reverse_string {
     }
     public static void main(String[] args) {
         char[] s = {'a','b','c'};
+        display(s);
+        reverseString(s);
         display(s);
     }
 }
