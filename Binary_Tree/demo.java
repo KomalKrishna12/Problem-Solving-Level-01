fc2d031d5c078efcf9c894208e515adea4eb83b9
KomalKrishna12 <komalkrishna016@gmail.com>
2022-03-05 14:31:59 +0530

    array list

--- Binary_Tree/demo.java
+++ Binary_Tree/demo.java
@@ -6,16 +6,14 @@ public class demo {
         
         ArrayList<Integer> list = new ArrayList<>() ;
 
-        list.add(5) ;
-        list.add(5) ;
-        list.add(5) ;
-        list.add(5) ;
+        list.add(1) ;
+        list.add(2) ;
+        list.add(3) ;
+        list.add(4) ;
         list.add(5) ;
 
         System.out.println(list);
-
-        list.add(2, 2);
-
+        list.add(0, 0);
         System.out.println(list);
 
     }
