064649cd11d97e2742cc6b061fc3a581025eee79
KomalKrishna12 <komalkrishna016@gmail.com>
2022-03-27 11:23:51 +0530

    climb stairs with min moves

--- Dynamic_Programming/dynamic_programming_and_greedy/Q4_Climb_Stairs_With_Minimum_Moves.java
+++ Dynamic_Programming/dynamic_programming_and_greedy/Q4_Climb_Stairs_With_Minimum_Moves.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
-
+// in this question we are required to find out min no of jumps to reach n
+// for every jump their is 1 move only(1 jump, 2 jump or 3 jump)
+// if we are at nth stair then no noeed to move so 0 move
+// store dp[n] = 0
+// create dp as Integer type so by default it contains null for all
+// if arr[i] > 0 then create a min variable and store the min value from its jump
 public class Q4_Climb_Stairs_With_Minimum_Moves {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
