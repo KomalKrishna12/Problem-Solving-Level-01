043e95ef261b0f960709c3279d46c5b0283cc71d
KomalKrishna12 <komalkrishna016@gmail.com>
2022-03-28 20:11:14 +0530

    coin change combinations

--- Dynamic_Programming/dynamic_programming_and_greedy/Q8_Coin_Change_Combination.java
+++ Dynamic_Programming/dynamic_programming_and_greedy/Q8_Coin_Change_Combination.java
@@ -1,6 +1,20 @@
 import java.util.Scanner;
+// we have given n coins these n no of coins are infinite we can use them any no of times
+// we have given a amount we have to count all the combinationa to collect target amount
+// so we create a dp array with length amt + 1 each index contains total no of combination
+// which can be made using n coins
+// so in outside place coins arr so we fill dp array for 1st coin then 2nd and all so we get
+// combinations not permuations
+// inside it start loop from arr[i] suppose we have coin 5 so we can make target from 5 to end
+// we cannot make 4 or less then 5 amount
+// now check if we add coin 5 so is their any possibility that we hvae j - 5
+// if yes then add that into dp 
+// suppose j is 7 coin is 5
+// so we are using coin 5 so check 2 is able to make any combination or not
+// if yes then add that combination into dp[7] 
+// steps :
 //to apply dp three steps required
-//1->storage and management
+//1->storage and meaning
 //2->direction identify
 //3->solve the problem by full filling the array
 public class Q8_Coin_Change_Combination {
