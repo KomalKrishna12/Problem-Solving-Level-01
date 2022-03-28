be9c4f380e5d22e383883c6c0b5003232049a8f1
KomalKrishna12 <komalkrishna016@gmail.com>
2022-03-28 21:16:21 +0530

    coin change permutations

--- Dynamic_Programming/dynamic_programming_and_greedy/Q9_Coin_Change_Permutations.java
+++ Dynamic_Programming/dynamic_programming_and_greedy/Q9_Coin_Change_Permutations.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
-
+// this is similar question like coin change combination but here we can print arrangements also
+// so in outer loop we place dp and in inner we place coins arr so for ith row
+// we try to calculate total arrangement which is possible to make i coins
 public class Q9_Coin_Change_Permutations {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
@@ -13,6 +15,8 @@ public class Q9_Coin_Change_Permutations {
         dp[0] = 1;
         for(int i = 1; i < dp.length; i++){
             for (int j = 0; j < arr.length; j++) {
+                // if jth coin is less than equal to target i then only we can use coin of j
+                // if i = 7 and coin is 8 so we cannot make tagrget 7 using coin 8
                 if(arr[j] <= i){
                     dp[i] += dp[i - arr[j]];
                 }
