6f4d8b423afc5e529890ce36a66c8ddf26aae1cf
KomalKrishna12 <komalkrishna016@gmail.com>
2022-06-05 10:38:33 +0530

    is graph connected

---
+++ graph/Q6_Is_Graph_Connected.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.util.*;
+// graph connected means any of vertex we can go till end
+// if graph is connected then only one component will be created
+public class Q6_Is_Graph_Connected {
+    static class Edge {
+        int src;
+        int nbr;
+        int wt;
+    
+        Edge(int src, int nbr, int wt) {
+          this.src = src;
+          this.nbr = nbr;
+          this.wt = wt;
+        }
+      }
+    
+      public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+        int vtces = Integer.parseInt(br.readLine());
+        ArrayList<ArrayList<Edge>> graph = new ArrayList<>();
+        for (int i = 0; i < vtces; i++) {
+          graph.add(new ArrayList<>());
+        }
+    
+        int edges = Integer.parseInt(br.readLine());
+        for (int i = 0; i < edges; i++) {
+          String[] parts = br.readLine().split(" ");
+          int v1 = Integer.parseInt(parts[0]);
+          int v2 = Integer.parseInt(parts[1]);
+          int wt = Integer.parseInt(parts[2]);
+          graph.get(v1).add(new Edge(v1, v2, wt));
+          graph.get(v2).add(new Edge(v2, v1, wt));
+        }
+    
+        ArrayList<ArrayList<Integer>> comps = new ArrayList<>();
+    
+        boolean[] visited = new boolean[vtces];
+        for (int v = 0; v < vtces; v++) {
+          if (visited[v] == false) {
+            ArrayList<Integer> comp = new ArrayList<>();
+            drawConnectedComponet(graph, v, comp, visited);
+            comps.add(comp);
+          }
+        }
+    
+        System.out.println(comps);
+
+        // now check comps size if it 1 then it is connected otherwise not
+
+        System.out.println(comps.size() == 1);
+        
+      }
+    
+      public static void drawConnectedComponet(ArrayList<ArrayList<Edge>> graph, int src, ArrayList<Integer> comp,
+          boolean[] visited) {
+    
+            visited[src] = true;
+            comp.add(src);
+            for(Edge edge : graph.get(src)){
+              if(visited[edge.nbr] == false){
+                drawConnectedComponet(graph, edge.nbr, comp, visited);
+              }
+            }
+    
+      }
+}
