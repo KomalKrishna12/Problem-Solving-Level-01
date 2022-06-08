0ca24fbdfb1dd52d8db8523b09e655aa1d572b38
KomalKrishna12 <komalkrishna016@gmail.com>
2022-06-08 10:03:15 +0530

    shortest path is weight

--- graph/Q15_Shortest_Path_in_Weights.java
+++ graph/Q15_Shortest_Path_in_Weights.java
@@ -1,5 +1,10 @@
 import java.io.*;
 import java.util.*;
+// this is Dijkstra Algorithm
+// similar like BFS
+// in BFS we use Queueu and here we use Priority Queue
+// in this ques we have tio find shortest path in terms of weight
+// so we create pair class and using comparable interface compare weight and find out path
 public class Q15_Shortest_Path_in_Weights {
     static class Edge {
         int src;
@@ -13,6 +18,21 @@ public class Q15_Shortest_Path_in_Weights {
         }
      }
   
+     static class Pair implements Comparable<Pair>{
+         int v;
+         String psf;
+         int wt;
+         Pair(int v, String psf, int wt){
+             this.v = v;
+             this.psf = psf;
+             this.wt = wt;
+         }
+         // we want to comapare weight of path so we return for weights
+        @Override
+        public int compareTo(Q15_Shortest_Path_in_Weights.Pair o) {
+            return this.wt - o.wt;
+        }
+     }
      public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
   
@@ -33,7 +53,31 @@ public class Q15_Shortest_Path_in_Weights {
         }
   
         int src = Integer.parseInt(br.readLine());
-        // write your code here
+        
+        boolean[] visited = new boolean[vtces];
+        PriorityQueue<Pair> queue = new PriorityQueue<>();
+        queue.add(new Pair(src, src + "", 0));
+
+        while(queue.size() > 0){
+            // remove
+            Pair remPair = queue.remove(); // this is will remove high priority pair which is shortest weight
+            
+            if(visited[remPair.v] == true) continue;
+
+            // mark visited if not
+            visited[remPair.v] = true;
+
+            // write
+            System.out.println(remPair.v + " via " + remPair.psf + " @ " + remPair.wt);
+
+            // add if there exists any nabour
+            for(Edge e : graph.get(remPair.v)){
+                if(visited[e.nbr] == false){
+                    queue.add(new Pair(e.nbr, remPair.psf + e.nbr, remPair.wt + e.wt));
+                }
+            }
+
+        }
         
      }
 }
